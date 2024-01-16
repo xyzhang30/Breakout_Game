e0883523ff28e92760c7dc1aceb655f694dcd7e9
Alisha Zhang <xz352@duke.edu>
2024-01-15 23:58:54 -0500

    added move() method to make paddle move according to mouse movement

--- src/main/java/breakout/Paddle.java
+++ src/main/java/breakout/Paddle.java
@@ -1,4 +1,22 @@
 package breakout;
 
-public class Paddle {
+import javafx.scene.shape.Rectangle;
+import javafx.scene.input.MouseEvent;
+
+public class Paddle extends Rectangle {
+    private double initialX; // Initial X-coordinate of the paddle
+
+    public Paddle(int Xupperleft, int Yupperleft, int paddleWidth, int paddleHeight){
+        super(Xupperleft, Yupperleft, paddleWidth, paddleHeight);
+        this.initialX = Xupperleft;
+    }
+
+    public void move(double mouseX) {
+        // Ensure the paddle stays within the scene bounds
+        double newX = mouseX - getWidth() / 2;
+        if (newX >= 0 && newX + getWidth() <= Main.SIZE) {
+            setX(newX);
+        }
+    }
+
 }

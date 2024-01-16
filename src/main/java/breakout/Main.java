322c149e963c25e06e3311f70f3e80803bdea195
Alisha Zhang <xz352@duke.edu>
2024-01-15 23:59:58 -0500

    added handleMouseMovement() method to make paddle move according to mouse movement

--- src/main/java/breakout/Main.java
+++ src/main/java/breakout/Main.java
@@ -5,10 +5,11 @@ import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
+//import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-import javafx.scene.shape.Circle;
+//import javafx.scene.shape.Circle;
 import javafx.util.Duration;
 
 
@@ -22,39 +23,54 @@ public class Main extends Application {
     public static final String TITLE = "Breakout Game"; //changed page title
     public static final Color DUKE_BLUE = new Color(0, 0.188, 0.529, 1);
     public static final Color BACKGROUND = new Color(0.3,0.3,0.3,1);
-    public static final int SIZE = 400;
+    public static final int SIZE = 500;
     public static final int SPEED = 100;
     public static final int FRAMES_PER_SECOND = 60;
     public static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
-    public Circle ball;
-
-
+    public static final int PADDLE_X = 200; //top left corner
+    public static final int PADDLE_Y = 480; //top left corner
+    public static final int PADDLE_WIDTH = 100;
+    public static final int PADDLE_HEIGHT = 10;
+    public Ball ball;
+    public Paddle paddle;
     /**
      * Initialize what will be displayed.
      */
     @Override
     public void start (Stage stage) {
-        ball = new Circle(20, 20, 20);
+        ball = new Ball(20, 20, 10);
         ball.setFill(Color.LIGHTSTEELBLUE);
+
+        paddle = new Paddle(PADDLE_X,PADDLE_Y,PADDLE_WIDTH,PADDLE_HEIGHT);
+        paddle.setFill(Color.BLACK);
 
         Group root = new Group();
         root.getChildren().add(ball);
-
+        root.getChildren().add(paddle);
 
         Scene scene = new Scene(root, SIZE, SIZE, BACKGROUND);
         stage.setScene(scene);
 
         stage.setTitle(TITLE);
         stage.show();
-        Timeline animation = new Timeline();
-        animation.setCycleCount(Timeline.INDEFINITE);
-        animation.getKeyFrames().add(new KeyFrame(Duration.seconds(SECOND_DELAY), e -> step(SECOND_DELAY)));
-        animation.play();
+        stage.setResizable(false);
+
+        scene.setOnMouseMoved(event -> handleMouseMove(event));
+
+
+//        Timeline animation = new Timeline();
+//        animation.setCycleCount(Timeline.INDEFINITE);
+//        animation.getKeyFrames().add(new KeyFrame(Duration.seconds(SECOND_DELAY), e -> step(SECOND_DELAY)));
+//        animation.play();
     }
 
-    private void step(double elapsedTime) {
-
-        ball.setCenterX(ball.getCenterX() + SPEED * elapsedTime);
-
+    private void handleMouseMove(MouseEvent event) {
+        paddle.move(event.getX());
     }
+
+//    private void step(double elapsedTime) {
+//
+//        ball.setCenterX(ball.getCenterX() + SPEED * elapsedTime);
+//
+//    }
 }

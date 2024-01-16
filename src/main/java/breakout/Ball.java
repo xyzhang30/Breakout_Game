4d82d95ad8612dfe34616498cf9ef733fbf4e1c8
Alisha Zhang <xz352@duke.edu>
2024-01-15 20:35:11 -0500

    created Ball class

---
+++ src/main/java/breakout/Ball.java
@@ -0,0 +1,11 @@
+package breakout;
+
+import javafx.scene.shape.Circle;
+import static breakout.Main.SPEED;
+
+public class Ball extends Circle {
+    double velocityX = SPEED;
+    double velocityY = SPEED;
+
+
+}

fe4051034eed00d86d8bfce65349297e2f928e33
Alisha Zhang <xz352@duke.edu>
2024-01-16 23:19:20 -0500

    added get/set velocity methods

--- src/main/java/breakout/Ball.java
+++ src/main/java/breakout/Ball.java
@@ -1,11 +1,40 @@
 package breakout;
 
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import static breakout.Main.SPEED;
+
 
 public class Ball extends Circle {
-    double velocityX = SPEED;
-    double velocityY = SPEED;
+    int velocityX = Main.SPEED;
+    int velocityY = Main.SPEED;
+
+    public Ball(double centerX, double CenterY, double radius, Color color) {
+        super(centerX,CenterY,radius,color);
+    }
+
+    public void setVelocityY(int newYvel) {
+        this.velocityY = newYvel;
+    }
+
+    public void setVelocityX(int newXvel){
+        this.velocityX = newXvel;
+    }
+
+    public int getVelocityX(){
+        return this.velocityX;
+    }
+
+    public int getVelocityY(){
+        return this.velocityY;
+    }
+
+    public void move(){
+
+    }
+
+    public void resetBall(){
+
+    }
 
 
 }

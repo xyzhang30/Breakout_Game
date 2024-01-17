52ba3d16ab57aedeac2ea9e5aac5b8bcae08649b
Alisha Zhang <xz352@duke.edu>
2024-01-16 23:22:05 -0500

    need to change mouse input to keycode input

--- src/main/java/breakout/Paddle.java
+++ src/main/java/breakout/Paddle.java
@@ -1,22 +1,27 @@
 package breakout;
 
+import javafx.scene.input.KeyCode;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.input.MouseEvent;
 
 public class Paddle extends Rectangle {
-    private double initialX; // Initial X-coordinate of the paddle
+    private double paddleX; // Initial X-coordinate of the paddle
 
     public Paddle(int Xupperleft, int Yupperleft, int paddleWidth, int paddleHeight){
         super(Xupperleft, Yupperleft, paddleWidth, paddleHeight);
-        this.initialX = Xupperleft;
+        this.paddleX = Xupperleft;
     }
 
-    public void move(double mouseX) {
-        // Ensure the paddle stays within the scene bounds
+    public void move(double mouseX) { //needs to move with key code
+        // set new X
         double newX = mouseX - getWidth() / 2;
         if (newX >= 0 && newX + getWidth() <= Main.SIZE) {
             setX(newX);
         }
+    }
+
+    public double getInitialX(){
+        return paddleX;
     }
 
 }

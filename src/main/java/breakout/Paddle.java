302bad9cde0fe92c1cf4589b6e4398a45cb725b2
Alisha Zhang <xz352@duke.edu>
2024-01-23 23:08:49 -0500

    add setPaddleX

--- src/main/java/breakout/Paddle.java
+++ src/main/java/breakout/Paddle.java
@@ -8,7 +8,6 @@ public class Paddle extends Rectangle {
     private double paddleX;
     private double paddleSpeed;
 
-
     public static final int SIZE = 540; //size of the screen
     public static final int PADDLE_INITIAL_X = SIZE/2-50;
     public static final int PADDLE_INITIAL_Y = SIZE - 20;
@@ -29,10 +28,6 @@ public class Paddle extends Rectangle {
         return paddleX;
     }
 
-//    public void stop() {
-//
-//    }
-
     public void moveLeft() {
         if (getX() - paddleSpeed >= 0) {
             setX(getX() - paddleSpeed);
@@ -43,5 +38,9 @@ public class Paddle extends Rectangle {
         if (getX() + getWidth() + paddleSpeed <= SIZE) {
             setX(getX() + paddleSpeed);
         }
+    }
+
+    public void setPaddleX(double newPaddleX){
+        this.setX(newPaddleX);
     }
 }
